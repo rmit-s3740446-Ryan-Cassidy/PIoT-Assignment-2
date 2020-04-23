27c76b835cf4ac0d12369fdef5bf2deb670fb911
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-04-23 22:28:40 +1000

    socketing

--- Task B/agent.py
+++ Task B/agent.py
@@ -1,12 +1,26 @@
 import socket
 import json
+import sys
 
-def loadconfig()
-    
+host = None
+port = None
+
+def loadconfig(self) :
+    try:
+        with open("config.json") as f:
+            data = json.load(f)
+            self.host = data["HOST"]
+            self.port = data["PORT"]
+    except Exception as e:
+        print(str(e))
+        sys.exit("Error when reading from Json.")
+
 # Socket to Master
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Connecting to Master")
-    s.bind()
+    s.bind(host, port)
+    s.listen(5)
+    conn, addr = s.accept()
 # Prompt user for login type
 # Send authentication attempt to Master
 # If true, sign in, else ask user again
