acb911fb16988397c44a987fb95af086c93d3adc
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-04-30 14:55:07 +1000

    modified config, added initial menu

--- Task B/agent.py
+++ Task B/agent.py
@@ -5,31 +5,32 @@ import threading
 import time
 from socketioClient import sioc
 
-host = None
-port = None
+ip = None
 
 def loadconfig(self) :
     try:
         with open("config.json") as f:
             data = json.load(f)
-            self.host = data["HOST"]
-            self.port = data["PORT"]
+            self.ip = "http://" + data["HOST"] + ":" + data["PORT"]
     except Exception as e:
         print(str(e))
         sys.exit("Error when reading from Json.")
 loadconfig
 # Socket to Master
-sioc.connect('http://192.168.1.225:5000')
+sioc.connect(ip)
 print('my sid is', sioc.sid)
 
-while True:
-    sioc.emit('message', 'hello there')
-    time.sleep(5)
-
+while True :
 # Prompt user for login type
-    # print("1. User Credentials")
-    # print("2. Facial Recognition")
-    # print("Select authentication method: ")
+    print("1. User Credentials")
+    print("2. Facial Recognition")
+    option = input("Select authentication method: ")
+    if option == '1' :
+        email = input('Please enter email: ')
+        password = input('Please enter password: ')
+    else :
+        exit("Other options NYI")
+    
 # User Credential Login
 # Facial Recognition Login
 # Send authentication attempt to Master
