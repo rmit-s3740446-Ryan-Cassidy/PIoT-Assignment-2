474718fdbc5b1bedbdcb132ac2be816ec15ecb81
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-05-17 15:15:48 +1000

    turned client into a class

--- Task-B/socketioClient.py
+++ Task-B/socketioClient.py
@@ -1,17 +1,18 @@
 import socketio
 
-sioc = socketio.Client()
+class socketClient:
+    sioc = socketio.Client()
 
-# Socket Events
+    # Socket Events
 
-@sioc.event
-def connect() :
-    print("Connection Successful")
+    @sioc.event
+    def connect(self) :
+        print("Connection Successful")
 
-@sioc.event
-def connect_error() :
-    print("Connection error")
+    @sioc.event
+    def connect_error(self) :
+        print("Connection error")
 
-@sioc.event
-def disconnect() :
-    print("Disconnected from server")
+    @sioc.event
+    def disconnect(self) :
+        print("Disconnected from server")
