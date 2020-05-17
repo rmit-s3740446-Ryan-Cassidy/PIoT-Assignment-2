49ad7ff45d73e4a8a860d2c57cf9cb493c523a64
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-05-17 15:16:21 +1000

    added unit testing events to socketserver added incorrect image added unit tests

--- Task-A/socketioServer.py
+++ Task-A/socketioServer.py
@@ -6,12 +6,12 @@ from recognize_face import recognize
 
 sios = SocketIO()
 
-# Reset agent event
+#Reset agent event
 @sios.on('reset')
 def handle_reset():
     return
 
-# User credential authentication event
+#User credential authentication event
 @sios.on('usercredauth')
 def handle_usercred(message):
     cars = []
@@ -42,16 +42,40 @@ def handle_facerec(message):
     else:
         return 'Fail', names[0], cars
 
-# Update Car status event
+#Update Car status event
 @sios.on('carupdatestatus')
 def handle_carupdatestatus(message):
     print('received updated car status')
     statusjson = {'id': message[0], 'status': message[1]}
     requests.post(request.host_url + "/updatecarstatus", json=statusjson)
 
-# Update Car location event
+#Update Car location event
 @sios.on('carupdatelocation')
 def handle_carupdatelocation(message):
     print('received updated car location')
     locationjson = {'id': message[0], 'location': message[1]}
     requests.post(request.host_url + "/updatecarlocation", json=locationjson)
+
+#Following events are for unit testing purposes
+
+#Event for testing credential login responses over socket
+@sios.on('usercredauth', namespace='/test')
+def handle_usercredtest(message):
+    userLoginData = {'username':message[0], 'password':message[1]}
+    authResponse = requests.post(request.host_url + "/loginUser", json=userLoginData)
+    authData = json.loads(authResponse.text)
+    if authData['message'] == 'Success':
+        return message[0]
+    else:
+        return 'Fail'
+
+#Event for testing facial recognition login response over socket
+@sios.on('facerecauth', namespace='/test')
+def handle_facerectest(message):
+    names = recognize(message)
+    existsResponse = requests.post(request.host_url + "/users/" + names[0])
+    exists = json.loads(existsResponse.text)
+    if exists['message'] == "True":
+        return 'Success'
+    else:
+        return 'Fail'
--- Task-B/unittests.py
+++ Task-B/unittests.py
@@ -1,46 +1,100 @@
 import unittest
-import agent
+import sys, json, base64, requests
+from pil import Image
+import time
+import socketio
 
 class Agent_Tests(unittest.TestCase):
+    client = socketio.Client() #Test socket client
+    ip = None
+    geolocateURL = None
 
+    #Config and Testing methods
+    def load_config(self) :
+        try:
+            with open("config.json") as f:
+                data = json.load(f)
+                self.ip = "http://" + data["host"] + ":" + data["port"]
+                self.geolocateURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + data['apikey']
+        except Exception as e:
+            print(str(e))
+            sys.exit("Error when reading from Json.")
+    
+    def image_to_str(self, imagepath) :
+        with open(imagepath, "rb") as image:
+            b64str = base64.b64encode(image.read())
+            return b64str
+
+    # Conntect to testing namespace
+    def connect(self) : 
+        self.client.connect(self.ip, namespaces=['/test'])
+
+    # Before All
     @classmethod
-    def setUpClass(cls):
-        agent.load_config()
-        agent.connect()
-
-    #Test Google Geolocation
-    def test_Geolocation_API(self):
-        response = requests.post(geolocateURL)
-        data = json.loads(response.text)
-        assertIn('location' in data)
+    def setUpClass(self):
+        self.load_config(self)
+        self.connect(self)
+    
+    # After All
+    @classmethod
+    def tearDownClass(self):
+        self.client.disconnect()
+        exit
+    
+    # After Each
+    # def tearDown(self):
+    #     self.username = None
     
     #Test connection to Master server by retrieving SID
     #SID is the ID assigned to a socket connection by Master
     def test_Socket_Connection(self):
-        sid = agent.sioc.sid
-        assertTrue(sid)
-    
+        sid = self.client.sid
+        self.assertTrue(sid)
+
+    #Test Master Login responses
+    #Tests use credential login test event
+
     #Test Login - Username/Password incorrect
     def test_Login_Incorrect(self):
         username = "thisis"
         password = "incorrect"
-        sioc.emit('usercredauth', [username, password])
+        auth = self.client.call('usercredauth', [username, password], namespace = '/test')
+        self.assertFalse(username == auth) #If login was successful, auth equals username, else 'Fail'
 
     #Test Login - Username correct, Password incorrect
     def test_Login_Username_Correct(self):
         username = "ryan"
         password = "incorrect"
+        auth = self.client.call('usercredauth', [username, password], namespace = '/test')
+        self.assertFalse(username == auth) #If login was successful, auth equals username, else 'Fail'
 
     #Test Login - Username correct, Password correct
     def test_Login_Correct(self):
         username = "ryan"
         password = "pass"
+        auth = self.client.call('usercredauth', [username, password], namespace = '/test')
+        self.assertTrue(username == auth) #If login was successful, auth equals username, else 'Fail'
 
-    #Test Facial Recognition Login - Correct Image
-    def test_Login_Facial_Match(self):
+    #OpenCV Testing
+    #Tests use facial recognition test event
 
     #Test Facial Recognition Login - Incorrect Image
     def testLogin_Facial_Mismatch(self):
+        image = self.image_to_str("wrongimage.jpg")
+        auth = self.client.call('facerecauth', image, namespace = '/test')
+        self.assertTrue(auth == 'Fail') #If image is not matched with encoding, auth equals 'Fail'
+    
+    #Test Facial Recognition Login - Correct Image
+    def test_Login_Facial_Match(self):
+        image = self.image_to_str("image.jpg")
+        auth = self.client.call('facerecauth', image, namespace = '/test')
+        self.assertTrue(auth == 'Success') #If image is matched with encoding, auth equals 'Success'
+    
+    #Test Google Geolocation
+    def test_Geolocation_API(self):
+        response = requests.post(self.geolocateURL)
+        data = json.loads(response.text)
+        self.assertIn('location', data) #If API key is set and request is returned. Data will contain location
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     unittest.main()
